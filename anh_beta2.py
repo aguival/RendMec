187b2088a444778fda67d5f758ee781188d10379
aguival <90341866+aguival@users.noreply.github.com>
2021-11-15 14:55:21 -0400

    Update anh_beta2.py

--- anh_beta2.py
+++ anh_beta2.py
@@ -1,3 +1,13 @@
+echo "# streamlit-to-heroku-tutorial" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/your_username/your_repo_name.git
+git push -u origin master
+
+
+
 import streamlit as st
 import pandas as pd
 import numpy as np
